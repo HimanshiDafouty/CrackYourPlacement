0966578952326872dfa8cfcff2370f21a0819069
Himanshi Dafouty <116953498+HimanshiDafouty@users.noreply.github.com>
2024-07-15 23:32:20 +0530

    Challenge-Day2

---
+++ Array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+        
+        return maxProfit;
+    }
+}
