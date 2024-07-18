b69b5f88ac1f885299b86d31054a57fcfa2e2da9
Himanshi Dafouty <116953498+HimanshiDafouty@users.noreply.github.com>
2024-07-18 12:21:46 +0530

    Challenge-Day5

---
+++ Tree/SymmetricTree.java
@@ -0,0 +1,20 @@
+class Solution {
+    private static boolean helper(TreeNode left ,TreeNode right)
+    {
+        if(left==null || right==null)
+        {
+            return left==right;
+        }
+        if(left.val != right.val)
+        {
+            return false;
+        }
+        return helper(left.left , right.right)
+        &&
+        helper(left.right , right.left);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return root==null || helper(root.left , root.right);
+        
+    }
+}
