bc1deaceaab0f11f13e00ffdfc4d1f4218f5fed1
Himanshi Dafouty <116953498+HimanshiDafouty@users.noreply.github.com>
2024-07-23 21:52:10 +0530

    Challenge-Day9

---
+++ Graph/DFS.java
@@ -0,0 +1,25 @@
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean[] visited =new boolean[V];
+        ArrayList<Integer> result= new ArrayList<>();
+        helper(0, visited ,result , adj );
+        return result;
+    }
+    void helper(int vertex , boolean[]visited , ArrayList<Integer> result,
+    ArrayList<ArrayList<Integer>> adjList){
+        // mark the current vertex as visited
+        visited[vertex]=true;
+        result.add(vertex);
+        
+        //explore the neighbours of current vertex
+        for(int i=0 ; i<adjList.get(vertex).size() ; i++){
+            int neighbour = adjList.get(vertex).get(i);
+            if(!visited[neighbour]){
+                helper(neighbour, visited , result, adjList);
+            }
+        }
+        
+    }
+}
